b11d7bc93d27d8bc7f11c09e4547b8604233759e
Madan Purohit <mspurohit09@gmail.com>
2021-01-27 13:44:43 +0530

    adding refresh button

--- src/Components/Coin/Coin.jsx
+++ src/Components/Coin/Coin.jsx
@@ -6,24 +6,29 @@ export default class Coin extends Component {
         this.state={
             price:this.props.price+100
         }
+        this.handleclick=this.handleclick.bind(this);
+
     }
-    // componentDidMount(){
-    //     const callback=()=>{
-    //         const randompercentage=0.995+Math.random()*0.01;
-    //         this.setState(function(oldstate){
-    //             return {
-    //                 price:oldstate.price*randompercentage
-    //             };
-    //         });
-    //     }
-    //     setInterval(callback,1000)
-    // }
+    handleclick(event){
+        event.preventDefault()
+            const randompercentage=0.995+Math.random()*0.01;
+            this.setState(function(oldstate){
+                return {
+                    price:oldstate.price*randompercentage
+                };
+            });
+        }   
     render() {
         return (
                 <tr className="coin-row">
                     <td>{this.props.name}</td>
                     <td>{this.props.ticker}</td>
                     <td>{this.state.price}</td>
+                    <td>
+                        <form action="#" method="post">
+                            <button onClick={this.handleclick}>Refresh</button>
+                        </form>
+                    </td>
                 </tr>
         )
     }
